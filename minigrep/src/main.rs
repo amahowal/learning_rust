b49eb7645f8df22d986c7a8d56d7579fa7813078
Austin Mahowald <austin.mahowald@gmail.com>
2023-11-27 08:53:00 -0800

    and refactored for modularity

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
-use std::fs;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     // The first item in the arguments list will be the path to the binary
@@ -21,39 +21,9 @@ fn main() {
     // run the logic
     // this error handling is similar to "unwrap_or_else" except that we return the unit type in
     // the success case so we only need to deal with an error, no need to unwrap a success returnj
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
-        process:exit(1);
+        process::exit(1);
     }
 }
 
-// Box is a trait object, so the error of the Result return can be anything with Error
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // The ? operator instead of expect will return the Error from whatever
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    // Unit type wrapped in Ok
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // magic number 3 because we have 3 arguments (first is exe pointer)
-        if args.len() < 3 {
-            // panic here is not reallyl the best thing to do, better to return Result
-            //panic!("Must specify all arguments (query, file_path)");
-            return Err("Must specify all arguments (query, file_path)");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
