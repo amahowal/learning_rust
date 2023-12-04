c7ddc1ceb440860c9e0082e78ee9bed18ece8886
Austin Mahowald <austin.mahowald@gmail.com>
2023-12-04 08:17:08 -0800

    section 15.2

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,3 +1,21 @@
+use std::ops::Deref;
+// implementing something similar to Box<T>
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -7,4 +25,11 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // Referencing review + smart pointers
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
