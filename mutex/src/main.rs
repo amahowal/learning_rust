55590db53b52610762b6ca3afefdb902db360ac3
Austin Mahowald <austin.mahowald@gmail.com>
2023-12-12 07:52:33 -0800

    using Arc to get around cuncurrency issues with Mutex

--- mutex/src/main.rs
+++ mutex/src/main.rs
@@ -1,13 +1,17 @@
-use std::rc::Rc;
-use std::sync::Mutex;
+// Just like using RefCell to mutate contents of Rc, we can use Mutex to change contents of Arc!
+// The compiler will protect against two threads using the same data at the same time, but it won't
+// protect against deadlocks. Logic errors could lead to two threads waiting for each other forever
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let counter = Rc::new(Mutex::new(0));
+    // unfortunately, Rc is not threadsafe, so we use Arc instead
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        //let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             // lock returns a MutexGuard in a LockResult
             let mut num = counter.lock().unwrap();
