6a534bb8ab65984a9a2fa2a10ed509d123713ab0
Austin Mahowald <austin.mahowald@gmail.com>
2023-12-19 07:08:09 -0800

    blog implemented

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -16,12 +16,64 @@ impl Post {
         self.content.push_str(text);
     }
     pub fn content(&self) -> &str {
+        // We can safely unwrap here because we know from the logic we wrote that we will never
+        // have a None value, even though the compiler doesn't know that
+        self.state.as_ref().unwrap().content(self)
+    }
+    pub fn request_review(&mut self) {
+        // Take moves the state value OUT of self.state, leaving a None in its place
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    // This is a default implementation returning an empty string (draft and pending review)
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
         ""
     }
 }
 
-trait State {}
-
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
